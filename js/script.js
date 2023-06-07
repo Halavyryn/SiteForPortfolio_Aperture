d65f40b48c6843244e1758a4ab7037176bd0421e
Halavyryn <andreihalavyryn@gmail.com>
2023-06-07 15:34:30 +0300

    Added Popups

--- js/script.js
+++ js/script.js
@@ -11,3 +11,16 @@ document.querySelectorAll(".nav-link").forEach(n =>
         hamburger.classList.remove("hamburger_active");
         Menu.classList.remove("menu_active");
     }))
+
+/*Popup for menu*/
+let popup = document.getElementById("popup");
+let text='';
+function openPopup(text){
+    popup.classList.add("open-popup");
+    popup.innerHTML = '<h2>'+text+'</h2><p>Sorry! Page under construction!</p><button type="button" onclick="closePopup()">OK</button>';
+        /*'<h2 class="success">'+text+'</h2>'*/
+}
+function closePopup(){
+    popup.classList.remove("open-popup");
+}
+
