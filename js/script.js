4a3e1d6c18c67572c67048e9a9b71ccb26e88302
Halavyryn <andreihalavyryn@gmail.com>
2023-06-07 20:10:15 +0300

    Added button Top

--- js/script.js
+++ js/script.js
@@ -24,3 +24,20 @@ function closePopup(){
     popup.classList.remove("open-popup");
 }
 
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTo({ top: 0, behavior: "smooth" });// For Safari
+    document.documentElement.scrollTo({ top: 0, behavior: "smooth" }); // For Chrome, Firefox, IE and Opera
+}
